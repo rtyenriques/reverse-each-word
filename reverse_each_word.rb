c9238c2f226294f235c4d4261188c606cf350a3b
rtyenriques <tooshopboys@gmail.com>
2021-01-18 22:38:04 -0800

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,21 +1,20 @@
 def reverse_each_word_with_each(string)
-    original_array = string.split(" ")
-    return_array = []
-    original_array.each do|string|
-      return_array << string.reverse
-    end
-    return_array.join(" ")
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
   end
-  
-  def reverse_each_word(string)
-    array = string.split(" ") 
-    test_array = []
-    array.collect do|string|
-      test_array << string.reverse
-    end
-    test_array.join(" ")
+  return_array.join(" ")
+end
+
+def reverse_each_word(string)
+  array = string.split(" ") 
+  test_array = []
+  array.collect do|string|
+    test_array << string.reverse
   end
-  
-  
-  reverse_each_word("hi there, and how are you?")
-  
+  test_array.join(" ")
+end
+
+
+reverse_each_word("hi there, and how are you?")
